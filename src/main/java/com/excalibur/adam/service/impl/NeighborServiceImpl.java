c7c943b91940272be062789ef11f36d2c1facae2
art1st-lh <art1st-lh@hotmail.com>
2018-04-30 22:51:21 +0800

    neighbor

---
+++ src/main/java/com/excalibur/adam/service/impl/NeighborServiceImpl.java
@@ -0,0 +1,29 @@
+package com.excalibur.adam.service.impl;
+
+import com.excalibur.adam.entity.Neighbor;
+import com.excalibur.adam.entity.UserAddress;
+import com.excalibur.adam.service.NeighborService;
+
+import java.util.List;
+
+/**
+ * @Author: Created by lh.
+ * @Date: 18/4/30
+ * @Time: 下午10:50
+ */
+public class NeighborServiceImpl implements NeighborService {
+    @Override
+    public List<Neighbor> findNeighbors(UserAddress userAddress) {
+        return null;
+    }
+
+    @Override
+    public void sayHello(String msg) {
+
+    }
+
+    @Override
+    public void sendFriendInvent() {
+
+    }
+}
